4d2f28f0e13a2b1e15cc845008400f0b19dc9fdd
Dominique Orban <dominique.orban@gmail.com>
2015-04-03 20:48:28 -0400

    CG: simplify test.

--- test/test_cg.jl
+++ test/test_cg.jl
@@ -5,7 +5,7 @@ cg_tol = 1.0e-6;
 # Cubic spline matrix.
 n = 10;
 A = spdiagm((ones(n-1), 4*ones(n), ones(n-1)), (-1, 0, 1))
-b = A * [1:10];
+b = A * [1:n];
 
 for mat in {A, full(A), LinearOperator(A)}
   (x, stats) = cg(mat, b, itmax=10);
@@ -21,13 +21,11 @@ end
 A = get_div_grad(16, 16, 16);
 b = randn(size(A, 1));
 
-for mat in {A, full(A), LinearOperator(A)}
-  (x, stats) = cg(A, b);
-  show(stats);
-  r = b - A * x;
-  resid = norm(r) / norm(b);
-  @printf("CG: Relative residual: %8.1e\n", resid);
-  @test(resid <= cg_tol);
-  @test(stats.solved);
-end
+(x, stats) = cg(A, b);
+show(stats);
+r = b - A * x;
+resid = norm(r) / norm(b);
+@printf("CG: Relative residual: %8.1e\n", resid);
+@test(resid <= cg_tol);
+@test(stats.solved);
 
