16173e857e2b50fa1bccfebaacdbf9d395e9a350
Dominique Orban <dominique.orban@gmail.com>
2017-01-20 16:02:13 -0500

    automatically define variants

--- src/cgls_methods.jl
+++
@@ -1,9 +0,0 @@
-cgls{TA <: Number, Tb <: Number}(A :: Array{TA,2}, b :: Array{Tb,1};
-                                 M :: AbstractLinearOperator=opEye(size(b,1)),
-                                 λ :: Float64=0.0, atol :: Float64=1.0e-8, rtol :: Float64=1.0e-6, itmax :: Int=0, verbose :: Bool=false) =
-  cgls(LinearOperator(A), b, M=M, λ=λ, atol=atol, rtol=rtol, itmax=itmax, verbose=verbose);
-
-cgls{TA <: Number, Tb <: Number, IA <: Integer}(A :: SparseMatrixCSC{TA,IA}, b :: Array{Tb,1};
-                                                M :: AbstractLinearOperator=opEye(size(b,1)),
-                                                λ :: Float64=0.0, atol :: Float64=1.0e-8, rtol :: Float64=1.0e-6, itmax :: Int=0, verbose :: Bool=false) =
-  cgls(LinearOperator(A), b, M=M, λ=λ, atol=atol, rtol=rtol, itmax=itmax, verbose=verbose);
