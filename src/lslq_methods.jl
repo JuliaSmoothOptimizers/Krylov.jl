ecc1c78a8e925abb32666c9f09bbe8e4cb62b24c
Dominique Orban <dominique.orban@gmail.com>
2017-01-20 13:24:30 -0500

    new method: lslq

--- src/lslq_methods.jl
+++ src/lslq_methods.jl
@@ -1,5 +1,7 @@
-lslq{TA <: Number, Tb <: Number}(A :: Array{TA,2}, b :: Array{Tb,1}; kwargs...) =
+"When `A` is given as an `Array`, it is first converted to a `LinearOperator`"
+lslq{TA <: Number, Tb <: Number}(A :: Array{TA,2}, b :: Vector{Tb}; kwargs...) =
   lslq(LinearOperator(A), b; kwargs...)
 
-lslq{TA <: Number, Tb <: Number, IA <: Integer}(A :: SparseMatrixCSC{TA,IA}, b :: Array{Tb,1}; kwargs...) =
+"When `A` is given as a `SparseMatrixCSC`, it is first converted to a `LinearOperator`"
+lslq{TA <: Number, Tb <: Number, IA <: Integer}(A :: SparseMatrixCSC{TA,IA}, b :: Vector{Tb}; kwargs...) =
   lslq(LinearOperator(A), b; kwargs...)
