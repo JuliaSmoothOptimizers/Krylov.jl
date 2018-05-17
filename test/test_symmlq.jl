55cf419afc86de407bbfaf8aa80043f003aea266
restrin <ronestrin756@gmail.com>
2018-05-17 14:11:10 -0700

    Added tests for error estimation

--- test/test_symmlq.jl
+++ test/test_symmlq.jl
@@ -58,3 +58,28 @@ resid = norm(r) / norm(b)
 A = spdiagm((ones(Int, n-1), 4*ones(Int, n), ones(Int, n-1)), (-1, 0, 1)); b = A * [1:n;]
 (x, xcg, stats) = symmlq(A, b)
 @test stats.solved
+
+# Test error estimate
+A = full(get_div_grad(8, 8, 8))
+b = ones(size(A, 1))
+λest = (1-1e-10)*eigmin(A)
+x_exact = A\b
+(x, xcg, stats) = symmlq(A, b, λest=λest)
+err = norm(x_exact - x)
+errcg = norm(x_exact - xcg)
+@printf("SYMMLQ    : true error: %8.1e\n", err)
+@printf("SYMMLQ-CG : true error: %8.1e\n", errcg)
+@printf("SYMMLQ    : err up-bnd : %8.1e\n", stats.errors[end])
+@printf("SYMMLQ-CG : err up-bnd : %8.1e\n", stats.errorscg[end])
+@test( err <= stats.errors[end] )
+@test( errcg <= stats.errorscg[end])
+(x, xcg, stats) = symmlq(A, b, λest=λest, window=1)
+@printf("SYMMLQ    : err up-bnd : %8.1e\n", stats.errors[end])
+@printf("SYMMLQ-CG : err up-bnd : %8.1e\n", stats.errorscg[end])
+@test( err <= stats.errors[end] )
+@test( errcg <= stats.errorscg[end])
+(x, xcg, stats) = symmlq(A, b, λest=λest, window=5)
+@printf("SYMMLQ    : err up-bnd : %8.1e\n", stats.errors[end])
+@printf("SYMMLQ-CG : err up-bnd : %8.1e\n", stats.errorscg[end])
+@test( err <= stats.errors[end] )
+@test( errcg <= stats.errorscg[end])
