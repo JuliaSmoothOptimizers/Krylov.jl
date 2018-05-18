364b8aa7912dfa9e458296c64f211a045ae71070
DahitoMA <marie-ange.dahito@polymtl.ca>
2018-05-18 12:15:55 -0400

    Check zero curvature

--- src/crls.jl
+++ src/crls.jl
@@ -86,19 +86,47 @@ function crls{T <: Number}(A :: AbstractLinearOperator, b :: Vector{T};
 
     # if a trust-region constraint is give, compute step to the boundary
     # (note that α > 0 in CRLS)
-    σ = radius > 0.0 ? maximum(to_boundary(x, p, radius)) : α
-    if (radius > 0.0) & (α > σ)
-      α = σ
-      on_boundary = true
-    end
+    if radius > 0.0
+      σ = maximum(to_boundary(x, p, radius))
+      ApNorm² = @knrm2(m, Ap)^2
+      if ApNorm² = 0 # q is linear in the direction p
+        spd = true # det(A'A) = 0
+        pAr = @kdot(n, p, Ar) # p'A'r
+        if pAr = 0 # q is constant in the direction p
+          p = Ar # p = A'r
+          q = A' * s
+          α = minimum(ArNorm² / (2 * γ), maximum(to_boundary(x, Ar, radius)) # q is minimal in the direction A'r for ν = ‖Ar‖²/(2γ)
+        else
+          descent = pAr > 0.0
+          if !descent 
+            σ = minimum(to_boundary(x, p, radius))
+          end
+          ArNorm² = ArNorm^2
+          ν = minimum(ArNorm² / (2 * γ), maximum(to_boundary(x, Ar, radius)) 
+          δ = -σ * pAr + ν * ArNorm² + σ^2 * ApNorm² - ν^2 * γ
+          if δ > 0.0
+            # direction A'r engenders a bigger decrease
+            p = Ar # p = A'r
+            q = A' * s
+            α = ν
+          else
+            α = σ
+          end
+        end
+        if α > σ
+          α = σ
+          on_boundary = true
+      end       
 
     @kaxpy!(n,  α, p,   x)     # Faster than  x =  x + α *  p;
-    @kaxpy!(m, -α, Ap,  r)     # Faster than  r =  r - α * Ap;
     @kaxpy!(n, -α, q,  Ar)     # Faster than Ar = Ar - α *  q;
+    ArNorm = @knrm2(n, Ar)
+    solved = ArNorm <= ε
+    (solved || on_boundary || spd) && continue
+    @kaxpy!(m, -α, Ap,  r)     # Faster than  r =  r - α * Ap;
     s = A * Ar;
     Ms = M * s;
     γ_next = @kdot(m, s, Ms)   # Faster than γ_next = dot(s, s);
-    ArNorm = @knrm2(n, Ar)
     λ > 0 && (γ_next += λ * ArNorm * ArNorm);
     β = γ_next / γ;
 
