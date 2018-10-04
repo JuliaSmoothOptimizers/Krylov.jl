888dbb79ccd844c3f5247fb2c749028ba9cd77a7
Xx-fractale-xX <alexis.montoison@polymtl.ca>
2018-10-04 02:05:09 -0400

    LU factorization with partial pivoting in DIOM

--- src/diom.jl
+++ src/diom.jl
@@ -39,6 +39,7 @@ function diom{T <: Number}(A :: AbstractLinearOperator, b :: AbstractVector{T};
 
   # Initial solution x₀ and residual r₀.
   x = zeros(T, n)
+  x_old = copy(x)
   r = copy(b)
   # Compute β.
   rNorm = @knrm2(n, r) # rNorm = ‖r₀‖
@@ -53,13 +54,14 @@ function diom{T <: Number}(A :: AbstractLinearOperator, b :: AbstractVector{T};
 
   # Set up workspace.
   mem = min(memory, itmax) # Memory.
-  P = zeros(n, mem-1) # Directions for x.
-  V = zeros(n, mem)   # Preconditioned Krylov vectors.
-  H = zeros(mem+1) # Last column of the band hessenberg matrix Hₘ = LₘUₘ.
-  # Each column has at most mem + 1 nonzero elements. hᵢ.ₘ is stored as H[m-i+2].
+  P = zeros(n, mem) # Directions for x.
+  V = zeros(n, mem) # Preconditioned Krylov vectors.
+  H = zeros(mem+2)  # Last column of the band hessenberg matrix Hₘ = LₘUₘ.
+  # Each column has at most mem + 2 nonzero elements. hᵢ.ₘ is stored as H[m-i+2].
   # m-i+2 represents the indice of the diagonal where hᵢ.ₘ is located.
   # In addition of that, the last column of Uₘ is stored in H.
   L = zeros(mem) # Last mem Pivots of Lₘ.
+  p = zeros(Bool, mem) # Last mem permutations.
 
   # Initial ξ and V.
   ξ = rNorm
@@ -82,6 +84,8 @@ function diom{T <: Number}(A :: AbstractLinearOperator, b :: AbstractVector{T};
     # Incomplete Arnoldi procedure.
     z = M * V[:,pos] # Forms pₘ
     w = A * z # Forms vₘ₊₁
+    # hₘ₋ₘₑₘ.ₘ = 0
+    H[mem+2] = 0
     for i = max(1, iter-mem+1) : iter
       ipos = mod(i-1, mem) + 1 # Position of vᵢ in the circular stack
       jpos = iter - i + 2
@@ -94,36 +98,25 @@ function diom{T <: Number}(A :: AbstractLinearOperator, b :: AbstractVector{T};
       V[:,next_pos] = w / H[1] # vₘ₊₁ = w / hₘ₊₁.ₘ
     end
 
-    # Update LU factorization of Hₘ by computing the last column of Uₘ and the next pivot lₘ₊₁.ₘ of Lₘ.
+    # Compute LU factorization with partial pivoting of Hₘ by computing the last column of Uₘ.
     if iter ≥ 2
-      # uᵢ.ₘ ← hᵢ.ₘ - lᵢ.ᵢ₋₁ * uᵢ₋₁.ₘ
-      for i = max(2,iter-mem+2) : iter
+      for i = max(2,iter-mem+1) : iter
         lpos = mod(i-1, mem) + 1 # Position of lᵢ.ᵢ₋₁ in the circular stack
         jpos = iter - i + 2
+        if p[lpos]
+          # row i-1 and i are permuted
+          H[jpos], H[jpos+1] = H[jpos+1], H[jpos]
+        end
+        # uᵢ.ₘ ← hᵢ.ₘ - lᵢ.ᵢ₋₁ * uᵢ₋₁.ₘ
         H[jpos] = H[jpos] - L[lpos] * H[jpos+1]
       end
       # Compute ξₘ the last composant of zₘ = β(Lₘ)⁻¹e₁.
-      # ξₘ = -lₘ.ₘ₋₁ * ξₘ₋₁
-      ξ = - L[pos] * ξ
+      if !p[pos] # p[pos] ⇒ ξₘ = ξₘ₋₁
+        # ξₘ = -lₘ.ₘ₋₁ * ξₘ₋₁
+        ξ = - L[pos] * ξ
+      end
     end
-    # lₘ₊₁.ₘ ← hₘ₊₁.ₘ / uₘ.ₘ
-    L[next_pos] = H[1] / H[2]
 
-    # Compute the last column of Pₘ = Vₘ(Uₘ)⁻¹.
-    for i = max(1,iter-mem+1) : iter - 1
-      ipos = mod(i-1, mem-1) + 1
-      jpos = iter - i + 2
-      # z ← z - uᵢ.ₘ * pᵢ
-      @kaxpy!(n, -H[jpos], P[:,ipos], z)
-    end
-    dpos = mod(iter-1, mem-1) + 1 # Position of pₘ in the circular stack
-    # pₘ = z / uₘ.ₘ
-    P[:,dpos] = z / H[2]
-
-    # Update solution x.
-    # xₘ ← xₘ₋₁ + ξₘ * pₘ
-    @kaxpy!(n, ξ, P[:,dpos], x)
-    
     # Update residual norm estimate.
     # ‖ b - Axₘ ‖ = hₘ₊₁.ₘ * |ξₘ / uₘ.ₘ|
     rNorm = H[1] * abs(ξ / H[2])
@@ -132,6 +125,44 @@ function diom{T <: Number}(A :: AbstractLinearOperator, b :: AbstractVector{T};
     # Update stopping criterion.
     solved = rNorm ≤ ε
     tired = iter ≥ itmax
+
+    # Compute the direction pₘ, the last column of Pₘ = Vₘ(Uₘ)⁻¹.
+    for i = max(1,iter-mem) : iter - 1
+      ipos = mod(i-1, mem) + 1
+      jpos = iter - i + 2
+      # z ← z - uᵢ.ₘ * pᵢ
+      @kaxpy!(n, -H[jpos], P[:,ipos], z)
+    end
+
+    # Determine if interchange between hₘ₊₁.ₘ and uₘ.ₘ is needed and compute next pivot lₘ₊₁.ₘ.
+    if abs(H[2]) < H[1]
+      p[next_pos] = true
+      # pₘ = z / hₘ₊₁.ₘ
+      P[:,pos] = z / H[1]
+      # lₘ₊₁.ₘ = uₘ.ₘ / hₘ₊₁.ₘ
+      L[next_pos] = H[2] / H[1]
+    else
+      p[next_pos] = false
+      # pₘ = z / uₘ.ₘ
+      P[:,pos] = z / H[2]
+      # lₘ₊₁.ₘ = hₘ₊₁.ₘ / uₘ.ₘ
+      L[next_pos] = H[1] / H[2]
+    end
+
+    # Compute solution xₘ.
+    if p[pos]
+      # xₘ = xₘ₋ₙ + ξₘ₋ₙ * pₘ
+      # m-n is the last iteration without permutation at the next step
+      x = x_old + ξ * P[:,pos]
+    else
+      # xₘ = xₘ₋₁ + ξₘ * pₘ
+      @kaxpy!(n, ξ, P[:,pos], x)
+    end
+
+    # Update x_old.
+    if !p[next_pos]
+      x_old = copy(x)
+    end
     verbose && @printf("%5d  %7.1e\n", iter, rNorm)
   end
   verbose && @printf("\n")
