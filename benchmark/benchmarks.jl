9a3dc428d645c4a1ef05116c81d4e121669a56d4
Dominique Orban <dominique.orban@gmail.com>
2018-10-27 10:52:36 -0400

    benchmark UFL matrices

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -4,9 +4,14 @@ using LinearAlgebra
 
 using Krylov
 using LinearOperators
+using MatrixMarket
 
 include("../test/get_div_grad.jl")
 include("../test/test_utils.jl")
+include("fetch_matrices.jl")
+
+# we don't want to use matrixdepot(matrix, :read) to read in matrices because of
+# https://github.com/JuliaMatrices/MatrixDepot.jl/issues/26
 
 const SUITE = BenchmarkGroup()
 
@@ -17,5 +22,43 @@ for N in [32, 64, 128]
   n = size(A, 1)
   b = ones(n)
   op = preallocated_LinearOperator(A)
-  SUITE["CG"]["DivGrad N=$N"] = @benchmarkable cg($op, $b)
+  M = nonallocating_opEye(n)
+  rtol = 1.0e-6
+  SUITE["CG"]["DivGrad N=$N"] = @benchmarkable cg($op, $b, M=$M, atol=0.0, rtol=$rtol, itmax=$n)
+end
+
+SUITE["CG"]["UFL-small"] = BenchmarkGroup()
+for matrix in spd_small
+  name = basename(matrix)
+  A = MatrixMarket.mmread(joinpath(matrix_path, "..", "data", "uf", matrix, "$(name).mtx"))
+  n = size(A, 1)
+  b = ones(n)
+  op = preallocated_LinearOperator(A)
+  M = nonallocating_opEye(n)
+  rtol = 1.0e-6
+  SUITE["CG"]["UFL-small"][matrix] = @benchmarkable cg($op, $b, M=$M, atol=0.0, rtol=$rtol, itmax=$n)
+end
+
+SUITE["CG"]["UFL-medium"] = BenchmarkGroup()
+for matrix in spd_med
+  name = basename(matrix)
+  A = MatrixMarket.mmread(joinpath(matrix_path, "..", "data", "uf", matrix, "$(name).mtx"))
+  n = size(A, 1)
+  b = ones(n)
+  op = preallocated_LinearOperator(A)
+  M = nonallocating_opEye(n)
+  rtol = 1.0e-6
+  SUITE["CG"]["UFL-medium"][matrix] = @benchmarkable cg($op, $b, M=$M, atol=0.0, rtol=$rtol, itmax=$n)
+end
+
+SUITE["CG"]["UFL-large"] = BenchmarkGroup()
+for matrix in spd_large
+  name = basename(matrix)
+  A = MatrixMarket.mmread(joinpath(matrix_path, "..", "data", "uf", matrix, "$(name).mtx"))
+  n = size(A, 1)
+  b = ones(n)
+  op = preallocated_LinearOperator(A)
+  M = nonallocating_opEye(n)
+  rtol = 1.0e-6
+  SUITE["CG"]["UFL-large"][matrix] = @benchmarkable cg($op, $b, M=$M, atol=0.0, rtol=$rtol, itmax=$n)
 end
