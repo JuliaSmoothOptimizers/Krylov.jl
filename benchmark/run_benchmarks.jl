1e134a738c83458c6496994936469e67210a3f6c
Dominique Orban <dominique.orban@gmail.com>
2018-11-10 11:33:06 -0500

    run_benchmarks: export all results to a gist

--- benchmark/run_benchmarks.jl
+++ benchmark/run_benchmarks.jl
@@ -3,24 +3,31 @@ using GitHub, JSON, PkgBenchmark
 commit = benchmarkpkg("Krylov")  # current state of repository
 master = benchmarkpkg("Krylov", "master")
 judgement = judge(commit, master)
-export_markdown("benchmark.md", judgement)
-# export_markdown("benchmark.md", commit)
+export_markdown("judgement.md", judgement)
+export_markdown("benchmark-commit.md", commit)
+export_markdown("benchmark-master.md", master)
 
 gist_json = JSON.parse(
 			"""
             {
-            "description": "A benchmark for Krylov repository",
-            "public": true,
-            "files": {
-                "benchmark.md": {
-                "content": "$(escape_string(sprint(export_markdown, commit)))"
-                }
-            }
+	            "description": "A benchmark for Krylov repository",
+	            "public": true,
+	            "files": {
+	                "judgement.md": {
+		                "content": "$(escape_string(sprint(export_markdown, judgement)))"
+	                },
+					"benchmark-commit.md": {
+		                "content": "$(escape_string(sprint(export_markdown, commit)))"
+	                },
+					"benchmark-master.md": {
+		                "content": "$(escape_string(sprint(export_markdown, master)))"
+	                }
+	            }
             }
             """
         )
 
 # Need to add GITHUB_AUTH to your .bashrc
-# myauth = GitHub.authenticate(ENV["GITHUB_AUTH"])
-# posted_gist = create_gist(params = gist_json, auth = myauth)
-# println(posted_gist.html_url)
+myauth = GitHub.authenticate(ENV["GITHUB_AUTH"])
+posted_gist = create_gist(params = gist_json, auth = myauth)
+println(posted_gist.html_url)
