6b25340af0b6b9001d346ebf1d928935372d1060
Alexis Montoison <alexis.montoison@polymtl.ca>
2019-04-10 21:40:11 -0400

    Multiprecision support for CR

--- src/cr.jl
+++ src/cr.jl
@@ -15,11 +15,11 @@ assumed to be symmetric and positive definite.
 In a linesearch context, 'linesearch' must be set to 'true'.
 """
 function cr(A :: AbstractLinearOperator, b :: AbstractVector{T};
-            M :: AbstractLinearOperator=opEye(), atol :: Float64=1.0e-8,
-            rtol :: Float64=1.0e-6, γ :: Float64=1.0e-6, itmax :: Int=0,
-            radius :: Float64=0.0, verbose :: Bool=false, linesearch :: Bool=false) where T <: Number
+            M :: AbstractLinearOperator=opEye(), atol :: T=√eps(T),
+            rtol :: T=√eps(T), γ :: T=√eps(T), itmax :: Int=0,
+            radius :: T=zero(T), verbose :: Bool=false, linesearch :: Bool=false) where T <: AbstractFloat
 
-  if linesearch && (radius > 0.0)
+  if linesearch && (radius > 0)
     error("'linesearch' set to 'true' but radius > 0")
   end
   n = size(b, 1) # size of the problem
@@ -28,15 +28,15 @@ function cr(A :: AbstractLinearOperator, b :: AbstractVector{T};
 
   # Initial state.
   x = zeros(T, n) # initial estimation x = 0
-  xNorm = 0.0
+  xNorm = zero(T)
   r = copy(M * b) # initial residual r = M * (b - Ax) = M * b
   Ar = A * r
   ρ = @kdot(n, r, Ar)
-  ρ == 0.0 && return (x, SimpleStats(true, false, [0.0], T[], "x = 0 is a zero-residual solution"))
+  ρ == 0 && return (x, SimpleStats(true, false, [zero(T)], T[], "x = 0 is a zero-residual solution"))
   p = copy(r)
   q = copy(Ar)
   if verbose
-    m = 0.0 # quadratic model
+    m = zero(T) # quadratic model
   end
 
   iter = 0
@@ -57,7 +57,7 @@ function cr(A :: AbstractLinearOperator, b :: AbstractVector{T};
   verbose && @printf("%5s %8s %8s %8s\n", "Iter", "‖x‖", "‖r‖", "quad")
   verbose && @printf("    %d  %8.1e %8.1e %8.1e\n", iter, xNorm, rNorm, m)
 
-  descent = pr > 0.0 # pᵀr > 0 means p is a descent direction
+  descent = pr > 0 # pᵀr > 0 means p is a descent direction
   solved = rNorm ≤ ε
   tired = iter ≥ itmax
   on_boundary = false
@@ -73,12 +73,12 @@ function cr(A :: AbstractLinearOperator, b :: AbstractVector{T};
         iter == 0 && return (b, stats)
         return (x, stats)
       end
-    elseif pAp ≤ 0.0 && radius == 0.0
+    elseif pAp ≤ 0 && radius == 0
       error("Indefinite system and no trust region")
     end
     Mq = M * q
 
-    if radius > 0.0
+    if radius > 0
       verbose && @printf("radius = %8.1e > 0 and ‖x‖ = %8.1e\n", radius, xNorm)
       # find t1 > 0 and t2 < 0 such that ‖x + ti * p‖² = radius²  (i = 1, 2)
       xNorm² = xNorm * xNorm
@@ -98,7 +98,7 @@ function cr(A :: AbstractLinearOperator, b :: AbstractVector{T};
           # q(x + αr) = q(x) - α ‖r‖² + ½ α² rᵀAr
           # 1) if rᵀAr > 0, the quadratic decreases from α = 0 to α = ‖r‖² / rᵀAr
           # 2) if rᵀAr ≤ 0, the quadratic decreases to -∞ in the direction r
-          if ρ > 0.0 # case 1
+          if ρ > 0 # case 1
             verbose && @printf("quadratic is convex in direction r, curv = %8.1e\n", ρ)
             α = min(tr, rNorm² / ρ)
           else # case 2
@@ -110,9 +110,9 @@ function cr(A :: AbstractLinearOperator, b :: AbstractVector{T};
           # q_r = q(x + α_r * r) - q(x) = -α_r * ‖r‖² + ½ (α_r)² * rᵀAr
           # Δ = q_p - q_r. If Δ > 0, r is followed, else p is followed
           α = descent ? t1 : t2
-          ρ > 0.0 && (tr = min(tr, rNorm² / ρ))
-          Δ = -α * pr + tr * rNorm² - 0.5 * (tr)^2 * ρ # as pᵀAp = 0
-          if Δ > 0.0 # direction r engenders a better decrease
+          ρ > 0 && (tr = min(tr, rNorm² / ρ))
+          Δ = -α * pr + tr * rNorm² - (tr)^2 * ρ / 2 # as pᵀAp = 0
+          if Δ > 0 # direction r engenders a better decrease
             verbose && @printf("direction r engenders a bigger decrease. q_p - q_r = %8.1e > 0\n", Δ)
             verbose && @printf("redefining p := r\n")
             p = r
@@ -123,7 +123,7 @@ function cr(A :: AbstractLinearOperator, b :: AbstractVector{T};
           end
         end
 
-      elseif pAp > 0.0 && ρ > 0.0 # no negative curvature
+      elseif pAp > 0 && ρ > 0 # no negative curvature
         verbose && @printf("positive curvatures along p and r. pᵀAp = %8.1e and rᵀAr = %8.1e\n", pAp, ρ)
         α = ρ / @kdot(n, q, Mq)
         if α ≥ t1
@@ -131,13 +131,13 @@ function cr(A :: AbstractLinearOperator, b :: AbstractVector{T};
           on_boundary = true
         end
 
-      elseif pAp > 0.0 && ρ < 0.0
+      elseif pAp > 0 && ρ < 0
         npcurv = true
         verbose && @printf("pᵀAp = %8.1e > 0 and rᵀAr = %8.1e < 0\n", pAp, ρ)
         # q_p is minimal for α_p = rᵀp / pᵀAp
         α = descent ?  min(t1, pr / pAp) : max(t2, pr / pAp)
-        Δ = -α * pr + tr * rNorm² + 0.5 * (α^2 * pAp - (tr)^2 * ρ)
-        if Δ > 0.0
+        Δ = -α * pr + tr * rNorm² + (α^2 * pAp - (tr)^2 * ρ) / 2
+        if Δ > 0
           verbose && @printf("direction r engenders a bigger decrease. q_p - q_r = %8.1e > 0\n", Δ)
           verbose && @printf("redefining p := r\n")
           p = r
@@ -147,13 +147,13 @@ function cr(A :: AbstractLinearOperator, b :: AbstractVector{T};
           verbose && @printf("direction p engenders an equal or a bigger decrease. q_p - q_r = %8.1e ≤ 0\n", Δ)
         end
 
-      elseif pAp < 0.0 && ρ > 0.0
+      elseif pAp < 0 && ρ > 0
         npcurv = true
         verbose && @printf("pᵀAp = %8.1e < 0 and rᵀAr = %8.1e > 0\n", pAp, ρ)
         α = descent ? t1 : t2
         tr = min(tr, rNorm² / ρ)
-        Δ = -α * pr + tr * rNorm² + 0.5 * (α^2 * pAp - (tr)^2 * ρ)
-        if Δ > 0.0
+        Δ = -α * pr + tr * rNorm² + (α^2 * pAp - (tr)^2 * ρ) / 2
+        if Δ > 0
           verbose && @printf("direction r engenders a bigger decrease. q_p - q_r = %8.1e > 0\n", Δ)
           verbose && @printf("redefining p := r\n")
           p = r
@@ -163,12 +163,12 @@ function cr(A :: AbstractLinearOperator, b :: AbstractVector{T};
           verbose && @printf("direction p engenders an equal or a bigger decrease. q_p - q_r = %8.1e ≤ 0\n", Δ)
         end
 
-      elseif pAp < 0.0 && ρ < 0.0
+      elseif pAp < 0 && ρ < 0
         npcurv = true
         verbose && @printf("negative curvatures along p and r. pᵀAp = %8.1e and rᵀAr = %8.1e\n", pAp, ρ)
         α = descent ? t1 : t2
-        Δ = -α * pr + tr * rNorm² + 0.5 * (α^2 * pAp - (tr)^2 * ρ)
-        if Δ > 0.0
+        Δ = -α * pr + tr * rNorm² + (α^2 * pAp - (tr)^2 * ρ) / 2
+        if Δ > 0
           verbose && @printf("direction r engenders a bigger decrease. q_p - q_r = %8.1e > 0\n", Δ)
           verbose && @printf("redefining p := r\n")
           p = r
@@ -179,11 +179,11 @@ function cr(A :: AbstractLinearOperator, b :: AbstractVector{T};
         end
       end
 
-    elseif radius == 0.0
+    elseif radius == 0
       α = ρ / @kdot(n, q, Mq) # step
     end
 
-    @kaxpy!(n,  α, p, x)
+    @kaxpy!(n, α, p, x)
     xNorm = @knrm2(n, x)
     xNorm ≈ radius && (on_boundary = true)
     @kaxpy!(n, -α, Mq, r) # residual
@@ -196,7 +196,7 @@ function cr(A :: AbstractLinearOperator, b :: AbstractVector{T};
 
     iter = iter + 1
     if verbose
-      m = m - α * pr + 0.5 * α^2 * pAp
+      m = m - α * pr + α^2 * pAp / 2
       @printf("    %d  %8.1e %8.1e %8.1e\n", iter, xNorm, rNorm, m)
     end
 
@@ -207,8 +207,8 @@ function cr(A :: AbstractLinearOperator, b :: AbstractVector{T};
     ρbar = ρ
     ρ = @kdot(n, r, Ar)
     β = ρ / ρbar # step for the direction computation
-    @kaxpby!(n, 1.0, r, β, p)
-    @kaxpby!(n, 1.0, Ar, β, q)
+    @kaxpby!(n, one(T), r, β, p)
+    @kaxpby!(n, one(T), Ar, β, q)
 
     pNorm² = rNorm² + 2 * β * pr - 2 * β * α * pAp + β^2 * pNorm²
     pNorm = sqrt(pNorm²)
@@ -216,7 +216,7 @@ function cr(A :: AbstractLinearOperator, b :: AbstractVector{T};
     abspr = abs(pr)
     pAp = ρ + β^2 * pAp # pᵀq
     abspAp = abs(pAp)
-    descent = pr > 0.0
+    descent = pr > 0
 
   end
   verbose && @printf("\n")
