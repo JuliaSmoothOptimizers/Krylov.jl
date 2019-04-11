c466fdc6770a1f2706cad4abda51f724400c0096
Alexis Montoison <alexis.montoison@polymtl.ca>
2019-04-10 22:50:05 -0400

    Multiprecision support for LSMR

--- test/test_mp.jl
+++ test/test_mp.jl
@@ -6,8 +6,9 @@ function test_mp()
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
   for fn in (:cg, :cgls, :usymqr, :cgne, :cgs, :crmr, :cg_lanczos,
-             :dqgmres, :diom, :cr, :lslq, :lsqr)
+             :dqgmres, :diom, :cr, :lslq, :lsqr, :lsmr)
     @printf("%10s ", string(fn))
     for T in (Float16, Float32, Float64, BigFloat)
       M = spdiagm(-1 => ones(T,n-1), 0 => 4*ones(T,n), 1 => ones(T,n-1))
