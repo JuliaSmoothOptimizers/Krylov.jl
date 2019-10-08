a8c0ad6f11e9882e2fef49470d34a8f8a8264c1c
Xx-Alexis-xX <alexis.montoison@etu.enseeiht.fr>
2019-10-08 15:21:37 -0400

    Update get_div_grad function

--- test/get_div_grad.jl
+++ test/get_div_grad.jl
@@ -2,19 +2,18 @@
 function get_div_grad(n1 :: Int, n2 :: Int, n3 :: Int)
 
   # Divergence
-  D1 = kron(Matrix(1.0I, n3, n3), kron(Matrix(1.0I, n2, n2), ddx(n1)))
-  D2 = kron(Matrix(1.0I, n3, n3), kron(ddx(n2),   Matrix(1.0I, n1, n1)))
-  D3 = kron(ddx(n3),   kron(Matrix(1.0I, n2, n2), Matrix(1.0I, n1, n1)))
+  D1 = kron(eye(n3), kron(eye(n2), ddx(n1)))
+  D2 = kron(eye(n3), kron(ddx(n2), eye(n1)))
+  D3 = kron(ddx(n3), kron(eye(n2), eye(n1)))
 
   # DIV from faces to cell-centers
   Div = [D1 D2 D3]
 
-  return Div * Div';
+  return Div * Div'
 end
 
 # 1D finite difference on staggered grid
 function ddx(n :: Int)
-  e = ones(n);
-  return d = sparse([1:n; 1:n], [1:n; 2:n+1], [-e; e])
+  e = ones(n)
+  return sparse([1:n; 1:n], [1:n; 2:n+1], [-e; e])
 end
-
