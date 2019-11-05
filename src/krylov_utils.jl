0db3edf323b75967dbd3d91a630d1012190de63e
Xx-Alexis-xX <alexis.montoison@etu.enseeiht.fr>
2019-11-05 16:25:45 -0500

    Add documentations

--- src/krylov_utils.jl
+++ src/krylov_utils.jl
@@ -1,6 +1,7 @@
-# Display an array in the form
-# [ -3.0e-01 -5.1e-01  1.9e-01 ... -2.3e-01 -4.4e-01  2.4e-01 ]
-# with ndisp/2 elements on each side.
+"""Display an array in the form
+[ -3.0e-01 -5.1e-01  1.9e-01 ... -2.3e-01 -4.4e-01  2.4e-01 ]
+with ndisp/2 elements on each side.
+"""
 function vec2str(x :: Array{T,1}; ndisp :: Int=7) where T <: Number
   n = length(x);
   if n <= ndisp
