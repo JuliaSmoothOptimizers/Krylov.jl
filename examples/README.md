2644485563295e5bbb846430e7d12e9cddec094b
Alexis Montoison <alexis.montoison@polymtl.ca>
2019-11-07 23:21:45 -0500

    Update examples for Julia 1.0

--- examples/README.md
+++ examples/README.md
@@ -4,5 +4,4 @@ Examples in this folder require other Julia packages:
 
 * [MatrixMarket.jl](https://github.com/JuliaSparse/MatrixMarket.jl)
 * [HarwellRutherfordBoeing.jl](https://github.com/JuliaSparse/HarwellRutherfordBoeing.jl)
-* [MUMPS.jl](https://github.com/dpo/MUMPS.jl)
-
+* [DistributedArrays.jl](https://github.com/JuliaParallel/DistributedArrays.jl)
--- examples/test_craig.jl
+++ examples/test_craig.jl
@@ -1,24 +1,21 @@
 using Krylov
-using LinearOperators
-using LinearAlgebra
-# using ProfileView
+using LinearAlgebra, Printf
 
-m = 5; n = 8;
-λ = 1.0e-3;
-A = rand(m,n); b = A * ones(n);
-xy_exact = [A  λ*I] \ b;  # In Julia, this is the min-norm solution!
+m = 5
+n = 8
+λ = 1.0e-3
+A = rand(m, n)
+b = A * ones(n)
+xy_exact = [A  λ*I] \ b # In Julia, this is the min-norm solution!
 
-(x, y, stats) = craig(A, b, λ=λ, atol=0.0, rtol=1.0e-20, verbose=true);
-# @profile (x, y, stats) = craig(A, b, λ=λ);
-
+(x, y, stats) = craig(A, b, λ=λ, atol=0.0, rtol=1.0e-20, verbose=true)
 show(stats)
+
 # Check that we have a minimum-norm solution.
 # When λ > 0 we solve min ‖(x,s)‖  s.t. Ax + λs = b, and we get s = λy.
-@printf("Primal feasibility: %7.1e\n", norm(b - A * x - λ^2 * y) / norm(b));
-@printf("Dual   feasibility: %7.1e\n", norm(x - A' * y) / norm(x));
-@printf("Error in x: %7.1e\n", norm(x - xy_exact[1:n]) / norm(xy_exact[1:n]));
+@printf("Primal feasibility: %7.1e\n", norm(b - A * x - λ^2 * y) / norm(b))
+@printf("Dual   feasibility: %7.1e\n", norm(x - A' * y) / norm(x))
+@printf("Error in x: %7.1e\n", norm(x - xy_exact[1:n]) / norm(xy_exact[1:n]))
 if λ > 0.0
-  @printf("Error in y: %7.1e\n", norm(λ * y - xy_exact[n+1:n+m]) / norm(xy_exact[n+1:n+m]));
+  @printf("Error in y: %7.1e\n", norm(λ * y - xy_exact[n+1:n+m]) / norm(xy_exact[n+1:n+m]))
 end
-
-# ProfileView.view()
