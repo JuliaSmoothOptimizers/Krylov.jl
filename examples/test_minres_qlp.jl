ce77360de51ddaefe1c833ab31649c2e5e5f868d
Alexis Montoison <alexis.montoison@polymtl.ca>
2020-02-19 22:15:21 -0500

    Check the solution of MINRES-QLP and SYMMLQ examples

--- examples/test_minres_qlp.jl
+++ examples/test_minres_qlp.jl
@@ -3,7 +3,7 @@ using LinearAlgebra, Printf
 
 A = diagm([1.0; 2.0; 3.0; 0.0])
 n = size(A, 1)
-b = ones(n)
+b = [1.0; 2.0; 3.0; 4.0]
 b_norm = norm(b)
 
 # MINRES-QLP returns the minimum-norm solution of symmetric, singular and inconsistent systems
@@ -13,3 +13,4 @@ r = b - A * x;
 @printf("Residual r: %s\n", Krylov.vec2str(r))
 @printf("Relative residual norm ‖r‖: %8.1e\n", norm(r) / b_norm)
 @printf("Solution x: %s\n", Krylov.vec2str(x))
+@printf("Minimum-norm solution? %s\n", x ≈ [1.0; 1.0; 1.0; 0.0])
