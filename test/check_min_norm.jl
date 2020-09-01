51170de5400714ffe0ff0caeffdc95d69a66eb0b
Alexis Montoison <alexis.montoison@polymtl.ca>
2020-08-31 22:24:22 -0400

    Improve check_min_norm function

--- test/check_min_norm.jl
+++ test/check_min_norm.jl
@@ -7,7 +7,8 @@ function check_min_norm(A, b, x; λ=0.0)
     AI = A
     xI = x
   end
-  xmin = AI' * ((AI * AI') \ b)
+  QR = qr(AI')
+  xmin = QR.Q * (QR.R' \ b)
   xmin_norm = norm(xmin)
   @printf("‖x - xmin‖ = %7.1e\n", norm(xI - xmin))
   return (xI, xmin, xmin_norm)
