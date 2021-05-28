39cdb14c028dd1a1157a1311bb8e2242c9ae1dc1
Alexis Montoison <alexis.montoison@polymtl.ca>
2021-05-28 12:39:50 -0400

    Add regularization in CRAIGMR

--- src/craigmr.jl
+++ src/craigmr.jl
@@ -5,7 +5,7 @@
 #
 # The method seeks to solve the minimum-norm problem
 #
-#  min ‖x‖²  s.t. Ax = b,
+#  min ‖x‖  s.t.  Ax = b,
 #
 # and is equivalent to applying the conjugate residual method
 # to the linear system
@@ -29,18 +29,18 @@ export craigmr, craigmr!
 
 """
     (x, y, stats) = craigmr(A, b::AbstractVector{T};
-                            M=I, N=I, λ::T=zero(T), atol::T=√eps(T),
+                            M=I, N=I, sqd::Bool=false, λ::T=zero(T), atol::T=√eps(T),
                             rtol::T=√eps(T), itmax::Int=0, verbose::Int=0, history::Bool=false) where T <: AbstractFloat
 
 Solve the consistent linear system
 
-    Ax + √λs = b
+    Ax + λs = b
 
-using the CRAIG-MR method, where λ ≥ 0 is a regularization parameter.
+using the CRAIGMR method, where λ ≥ 0 is a regularization parameter.
 This method is equivalent to applying the Conjugate Residuals method
 to the normal equations of the second kind
 
-    (AAᵀ + λI) y = b
+    (AAᵀ + λ²I)y = b
 
 but is more stable. When λ = 0, this method solves the minimum-norm problem
 
@@ -48,16 +48,23 @@ but is more stable. When λ = 0, this method solves the minimum-norm problem
 
 When λ > 0, this method solves the problem
 
-    min ‖(x,s)‖₂  s.t. Ax + √λs = b.
+    min ‖(x,s)‖₂  s.t.  Ax + λs = b.
 
-Preconditioners M⁻¹ and N⁻¹ may be provided in the form of linear operators and are
-assumed to be symmetric and positive definite.
-Afterward CRAIGMR solves the symmetric and quasi-definite system
+If `sqd = true`, CRAIGMR solves the symmetric and quasi-definite system
 
-    [ -N   Aᵀ ] [ x ]   [ 0 ]
-    [  A   M  ] [ y ] = [ b ],
+    [ -F   Aᵀ ] [ x ]   [ 0 ]
+    [  A   E  ] [ y ] = [ b ],
 
-which is equivalent to applying MINRES to (M + AN⁻¹Aᵀ)y = b.
+where E and F are symmetric and positive definite.
+CRAIGMR is then equivalent to applying MINRES to `(AF⁻¹Aᵀ + E)y = b` with `Fx = Aᵀy`.
+Preconditioners M = E⁻¹ ≻ 0 and N = F⁻¹ ≻ 0 may be provided in the form of linear operators.
+
+If `sqd = false`, CRAIGMR solves the symmetric and indefinite system
+
+    [ -F   Aᵀ ] [ x ]   [ 0 ]
+    [  A   0  ] [ y ] = [ b ].
+
+In this case, M can still be specified and indicates the weighted norm in which residuals are measured.
 
 CRAIGMR produces monotonic residuals ‖r‖₂.
 It is formally equivalent to CRMR, though can be slightly more accurate,
@@ -75,7 +82,7 @@ function craigmr(A, b :: AbstractVector{T}; kwargs...) where T <: AbstractFloat
 end
 
 function craigmr!(solver :: CraigmrSolver{T,S}, A, b :: AbstractVector{T};
-                  M=I, N=I, λ :: T=zero(T), atol :: T=√eps(T),
+                  M=I, N=I, sqd :: Bool=false, λ :: T=zero(T), atol :: T=√eps(T),
                   rtol :: T=√eps(T), itmax :: Int=0, verbose :: Int=0, history :: Bool=false) where {T <: AbstractFloat, S <: DenseVector{T}}
 
   m, n = size(A)
@@ -95,10 +102,14 @@ function craigmr!(solver :: CraigmrSolver{T,S}, A, b :: AbstractVector{T};
   # Compute the adjoint of A
   Aᵀ = A'
 
+  # When solving a SQD system, set regularization parameter λ = 1.
+  sqd && (λ = one(T))
+
   # Set up workspace.
   allocate_if(!MisI, solver, :u, S, m)
   allocate_if(!NisI, solver, :v, S, n)
-  x, Nv, Aᵀu, y, Mu, w, wbar, Av = solver.x, solver.Nv, solver.Aᵀu, solver.y, solver.Mu, solver.w, solver.wbar, solver.Av
+  allocate_if(λ > 0, solver, :q, S, n)
+  x, Nv, Aᵀu, d, y, Mu, w, wbar, Av, q = solver.x, solver.Nv, solver.Aᵀu, solver.d, solver.y, solver.Mu, solver.w, solver.wbar, solver.Av, solver.q
   u = MisI ? Mu : solver.u
   v = NisI ? Nv : solver.v
 
@@ -132,9 +143,22 @@ function craigmr!(solver :: CraigmrSolver{T,S}, A, b :: AbstractVector{T};
   @kscal!(n, one(T)/α, v)
   NisI || @kscal!(n, one(T)/α, Nv)
 
+  # Regularization.
+  λₖ  = λ             # λ₁ = λ
+  cpₖ = spₖ = one(T)  # Givens sines and cosines used to zero out λₖ
+  cdₖ = sdₖ = one(T)  # Givens sines and cosines used to define λₖ₊₁
+  λ > 0 && (q .= v)   # Additional vector needed to update x, by definition q₀ = 0
+
+  if λ > 0
+    (cpₖ, spₖ, αhat) = sym_givens(α, λₖ)
+    @kscal!(n, spₖ, q)  # q̄₁ = sp₁ * v₁
+  else
+    αhat = α
+  end
+
   # Initialize other constants.
   ζbar = β
-  ρbar = α
+  ρbar = αhat
   θ = zero(T)
   rNorm = ζbar
   rNorms = history ? [rNorm] : T[]
@@ -145,8 +169,9 @@ function craigmr!(solver :: CraigmrSolver{T,S}, A, b :: AbstractVector{T};
   ɛ_i = atol + rtol * ArNorm  # Stopping tolerance for inconsistent systems.
 
   wbar .= u
-  @kscal!(m, one(T)/α, wbar)
+  @kscal!(m, one(T)/αhat, wbar)
   w .= zero(T)
+  d .= zero(T)
 
   status = "unknown"
   solved = rNorm ≤ ɛ_c
@@ -169,6 +194,13 @@ function craigmr!(solver :: CraigmrSolver{T,S}, A, b :: AbstractVector{T};
 
     Anorm² = Anorm² + β * β  # = ‖B_{k-1}‖²
 
+    if λ > 0
+      βhat = cpₖ * β
+      λₐᵤₓ = spₖ * β
+    else
+      βhat = β
+    end
+
     # Continue QR factorization
     #
     # Q [ Lₖ  β₁ e₁ ] = [ Rₖ   zₖ  ] :
@@ -182,14 +214,35 @@ function craigmr!(solver :: CraigmrSolver{T,S}, A, b :: AbstractVector{T};
     #
     # [ c  s ] [ ζbar ] = [ ζ     ]
     # [ s -c ] [  0   ]   [ ζbar⁺ ]
-    (c, s, ρ) = sym_givens(ρbar, β)
+    (c, s, ρ) = sym_givens(ρbar, βhat)
     ζ = c * ζbar
     ζbar = s * ζbar
     rNorm = abs(ζbar)
     history && push!(rNorms, rNorm)
 
     @kaxpby!(m, one(T)/ρ, wbar, -θ/ρ, w)  # w = (wbar - θ * w) / ρ
-    @kaxpy!(m, ζ, w, y)             # y = y + ζ * w
+    @kaxpy!(m, ζ, w, y)                   # y = y + ζ * w
+
+    if λ > 0
+      # DₖRₖ = V̅ₖ with v̅ₖ = cpₖvₖ + spₖqₖ₋₁
+      if iter == 1
+        @kaxpy!(n, one(T)/ρ, cpₖ * v, d)
+      else
+        @kaxpby!(n, one(T)/ρ, cpₖ * v, -θ/ρ, d)
+        @kaxpy!(n, one(T)/ρ, spₖ * q, d)
+        @kaxpby!(n, spₖ, v, -cpₖ, q)  # q̄ₖ ← spₖ * vₖ - cpₖ * qₖ₋₁
+      end
+    else
+      # DₖRₖ = Vₖ
+      if iter == 1
+        @kaxpy!(n, one(T)/ρ, v, d)
+      else
+        @kaxpby!(n, one(T)/ρ, v, -θ/ρ, d)
+      end
+    end
+
+    # xₖ = Dₖzₖ
+    @kaxpy!(n, ζ, d, x)
 
     # 2. αₖ₊₁Nvₖ₊₁ = Aᵀuₖ₊₁ - βₖ₊₁Nvₖ
     mul!(Aᵀu, Aᵀ, u)
@@ -202,25 +255,27 @@ function craigmr!(solver :: CraigmrSolver{T,S}, A, b :: AbstractVector{T};
 
     display(iter, verbose) && @printf("%5d  %7.1e  %7.1e  %7.1e  %7.1e  %8.1e  %8.1e  %7.1e\n", 1 + 2 * iter, rNorm, ArNorm, β, α, c, s, Anorm²)
 
+    if λ > 0
+      (cdₖ, sdₖ, λₖ₊₁) = sym_givens(λ, λₐᵤₓ)
+      @kscal!(n, sdₖ, q)  # qₖ ← sdₖ * q̄ₖ
+      (cpₖ, spₖ, αhat) = sym_givens(α, λₖ₊₁)
+    else
+      αhat = α
+    end
+
     if α ≠ 0
       @kscal!(n, one(T)/α, v)
       NisI || @kscal!(n, one(T)/α, Nv)
-      @kaxpby!(m, one(T)/α, u, -β/α, wbar)  # wbar = (u - beta * wbar) / alpha
+      @kaxpby!(m, one(T) / αhat, u, -βhat / αhat, wbar)  # wbar = (u - beta * wbar) / alpha
     end
-    θ = s * α
-    ρbar = -c * α
+    θ    =  s * αhat
+    ρbar = -c * αhat
 
     solved = rNorm ≤ ɛ_c
     inconsistent = (rNorm > 100 * ɛ_c) & (ArNorm ≤ ɛ_i)
     tired  = iter ≥ itmax
   end
   (verbose > 0) && @printf("\n")
-
-  Aᵀy = Aᵀu
-  mul!(Aᵀy, Aᵀ, y)
-  N⁻¹Aᵀy = NisI ? Aᵀy : solver.v
-  mul!(N⁻¹Aᵀy, N, Aᵀy)
-  @. x = N⁻¹Aᵀy
 
   status = tired ? "maximum number of iterations exceeded" : (solved ? "found approximate minimum-norm solution" : "found approximate minimum least-squares solution")
   stats = SimpleStats(solved, inconsistent, rNorms, ArNorms, status)
