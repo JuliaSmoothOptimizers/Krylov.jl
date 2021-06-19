88387c9070f7c1587ea1de31401c464b8c5f76f6
tmigot <tangi.migot@gmail.com>
2021-06-19 05:49:56 +0200

    fix/add tests

--- test/test_lnlq.jl
+++ test/test_lnlq.jl
@@ -2,7 +2,7 @@
   lnlq_tol = 1.0e-6
 
   function test_lnlq(A, b,transfer_to_craig)
-    (x, y, stats) = lnlq(A, b, transfer_to_craig=transfer_to_craig)
+    (x, y, stats) = lnlq(A, b, transfer_to_craig=transfer_to_craig, etolx=0.0, etoly=0.0)
     r = b - A * x
     resid = norm(r) / norm(b)
     return (x, y, stats, resid)
@@ -28,6 +28,9 @@
     @test(stats.solved)
     (xI, xmin, xmin_norm) = check_min_norm(A, b, x)
     @test(norm(xI - xmin) ≤ cond(A) * lnlq_tol * xmin_norm)
+    (xₛ, yₛ, stats) = lnlq(A, b, transfer_to_craig=transfer_to_craig, atol=0.0, rtol=0.0, σₐ=0.5)
+    (xI, xmin, xmin_norm) = check_min_norm(A, b, xₛ)
+    @test(norm(xI - xmin) ≤ cond(A) * lnlq_tol * xmin_norm)
 
     # Square consistent.
     A, b = square_consistent()
@@ -36,6 +39,9 @@
     @test(resid ≤ lnlq_tol)
     @test(stats.solved)
     (xI, xmin, xmin_norm) = check_min_norm(A, b, x)
+    @test(norm(xI - xmin) ≤ cond(A) * lnlq_tol * xmin_norm)
+    (xₛ, yₛ, stats) = lnlq(A, b, transfer_to_craig=transfer_to_craig, atol=0.0, rtol=0.0, σₐ=0.5)
+    (xI, xmin, xmin_norm) = check_min_norm(A, b, xₛ)
     @test(norm(xI - xmin) ≤ cond(A) * lnlq_tol * xmin_norm)
 
     # Overdetermined consistent.
@@ -46,48 +52,63 @@
     @test(stats.solved)
     (xI, xmin, xmin_norm) = check_min_norm(A, b, x)
     @test(norm(xI - xmin) ≤ cond(A) * lnlq_tol * xmin_norm)
+    (xₛ, yₛ, stats) = lnlq(A, b, transfer_to_craig=transfer_to_craig, atol=0.0, rtol=0.0, σₐ=0.5)
+    (xI, xmin, xmin_norm) = check_min_norm(A, b, xₛ)
+    @test(norm(xI - xmin) ≤ cond(A) * lnlq_tol * xmin_norm)
 
     # Test regularization
     A, b, λ = regularization()
-    (x, y, stats) = lnlq(A, b, λ=λ, transfer_to_craig=transfer_to_craig)
-    s = λ * y
-    r = b - (A * x + λ * s)
-    resid = norm(r) / norm(b)
-    @test(resid ≤ lnlq_tol)
-    r2 = b - (A * A' + λ^2 * I) * y
-    resid2 = norm(r2) / norm(b)
-    @test(resid2 ≤ lnlq_tol)
+    (x, y, stats) = lnlq(A, b, λ=λ, transfer_to_craig=transfer_to_craig, etolx=0.0, etoly=0.0)
+    (xₛ, yₛ, stats) = lnlq(A, b, transfer_to_craig=transfer_to_craig, atol=0.0, rtol=0.0, etolx=1e-10, etoly=1e-10, λ=λ)
+    for (x, y) in ((x, y), (xₛ, yₛ))
+      s = λ * y
+      r = b - (A * x + λ * s)
+      resid = norm(r) / norm(b)
+      @test(resid ≤ lnlq_tol)
+      r2 = b - (A * A' + λ^2 * I) * y
+      resid2 = norm(r2) / norm(b)
+      @test(resid2 ≤ lnlq_tol)
+    end
 
     # Test saddle-point systems
     A, b, D = saddle_point()
     D⁻¹ = inv(D)
     (x, y, stats) = lnlq(A, b, N=D⁻¹, transfer_to_craig=transfer_to_craig)
-    r = b - A * x
-    resid = norm(r) / norm(b)
-    @test(resid ≤ lnlq_tol)
-    r2 = b - (A * D⁻¹ * A') * y
-    resid2 = norm(r2) / norm(b)
-    @test(resid2 ≤ lnlq_tol)
+    (xₛ, yₛ, stats) = lnlq(A, b, N=D⁻¹, transfer_to_craig=transfer_to_craig, atol=0.0, rtol=0.0, σₐ=0.001)
+    for (x, y) in ((x, y), (xₛ, yₛ))
+      r = b - A * x
+      resid = norm(r) / norm(b)
+      @test(resid ≤ lnlq_tol)
+      r2 = b - (A * D⁻¹ * A') * y
+      resid2 = norm(r2) / norm(b)
+      @test(resid2 ≤ lnlq_tol)
+    end
 
     # Test with preconditioners
     A, b, M⁻¹, N⁻¹ = two_preconditioners()
     (x, y, stats) = lnlq(A, b, M=M⁻¹, N=N⁻¹, sqd=false, transfer_to_craig=transfer_to_craig)
-    r = b - A * x
-    resid = sqrt(dot(r, M⁻¹ * r)) / norm(b)
-    @test(resid ≤ lnlq_tol)
-    @test(norm(x - N⁻¹ * A' * y) ≤ lnlq_tol * norm(x))
+    (xₛ, yₛ, stats) = lnlq(A, b, M=M⁻¹, N=N⁻¹, sqd=false, transfer_to_craig=transfer_to_craig, atol=0.0, rtol=0.0, σₐ=0.5)
+    for (x, y) in ((x, y), (xₛ, yₛ))
+      r = b - A * x
+      resid = sqrt(dot(r, M⁻¹ * r)) / norm(b)
+      @test(resid ≤ lnlq_tol)
+      @test(norm(x - N⁻¹ * A' * y) ≤ lnlq_tol * norm(x))
+    end
 
     # Test symmetric and quasi-definite systems
     A, b, M, N = sqd()
     M⁻¹ = inv(M)
     N⁻¹ = inv(N)
     (x, y, stats) = lnlq(A, b, M=M⁻¹, N=N⁻¹, sqd=true, transfer_to_craig=transfer_to_craig)
-    r = b - (A * x + M * y)
-    resid = norm(r) / norm(b)
-    @test(resid ≤ lnlq_tol)
-    r2 = b - (A * N⁻¹ * A' + M) * y
-    resid2 = norm(r2) / norm(b)
-    @test(resid2 ≤ lnlq_tol)
+    (xₛ, yₛ, stats) = lnlq(A, b, M=M⁻¹, N=N⁻¹, sqd=true, transfer_to_craig=transfer_to_craig, atol=0.0, rtol=0.0, σₐ=0.5)
+    for (x, y) in ((x, y), (xₛ, yₛ))
+      r = b - (A * x + M * y)
+      resid = norm(r) / norm(b)
+      @test(resid ≤ lnlq_tol)
+      r2 = b - (A * N⁻¹ * A' + M) * y
+      resid2 = norm(r2) / norm(b)
+      @test(resid2 ≤ lnlq_tol)
+    end
 
     # Test dimension of additional vectors
     for transpose ∈ (false, true)
