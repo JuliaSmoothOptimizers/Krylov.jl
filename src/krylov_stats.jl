51174cd9a13e4b7da0cbe65ec1b416b715685737
tmigot <tangi.migot@gmail.com>
2021-06-24 17:22:25 +0200

    generic show fct

--- src/krylov_stats.jl
+++ src/krylov_stats.jl
@@ -75,46 +75,31 @@ end
 
 import Base.show
 
-function show(io :: IO, stats :: SimpleStats)
-  s  = "\nSimple stats\n"
-  s *= @sprintf("  solved: %s\n", stats.solved)
-  s *= @sprintf("  inconsistent: %s\n", stats.inconsistent)
-  s *= @sprintf("  residuals:  %s\n", vec2str(stats.residuals))
-  s *= @sprintf("  Aresiduals: %s\n", vec2str(stats.Aresiduals))
-  s *= @sprintf("  status: %s\n", stats.status)
-  print(io, s)
-end
+special_fields = Dict(
+  :residualscg => "residuals (cg)",
+  :errorscg => "errors (cg)",
+  :Anorm => "‖A‖F",
+  :Acond => "κ₂(A)",
+)
 
-function show(io :: IO, stats :: LanczosStats)
-  s  = "\nCG Lanczos stats\n"
-  s *= @sprintf("  solved: %s\n", stats.solved)
-  s *= @sprintf("  residuals: %s\n", typeof(stats.residuals))
-  s *= @sprintf("  flagged: %s\n", stats.flagged)
-  s *= @sprintf("  ‖A‖F: %7.1e\n", stats.Anorm)
-  s *= @sprintf("  κ₂(A): %7.1e\n", stats.Acond)
-  s *= @sprintf("  status: %s\n", stats.status)
-  print(io, s)
-end
-
-function show(io :: IO, stats :: SymmlqStats)
-  s  = "\nSYMMLQ stats\n"
-  s *= @sprintf("  solved: %s\n", stats.solved)
-  s *= @sprintf("  residuals: %s\n", vec2str(stats.residuals))
-  s *= @sprintf("  residuals (cg): %s\n", vec2str(stats.residualscg))
-  s *= @sprintf("  errors: %s\n", vec2str(stats.errors))
-  s *= @sprintf("  errors(cg): %s\n", vec2str(stats.errorscg))
-  s *= @sprintf("  ‖A‖F: %7.1e\n", stats.Anorm)
-  s *= @sprintf("  κ₂(A): %7.1e\n", stats.Acond)
-  s *= @sprintf("  status: %s\n", stats.status)
-  print(io, s)
-end
-
-function show(io :: IO, stats :: AdjointStats)
-  s  = "\nAdjoint stats\n"
-  s *= @sprintf("  solved primal: %s\n", stats.solved_primal)
-  s *= @sprintf("  solved dual:   %s\n", stats.solved_dual)
-  s *= @sprintf("  residuals primal: %s\n", vec2str(stats.residuals_primal))
-  s *= @sprintf("  residuals dual:   %s\n", vec2str(stats.residuals_dual))
-  s *= @sprintf("  status: %s\n", stats.status)
-  print(io, s)
+for f in ["Simple", "Lanczos", "Symmlq", "Adjoint"]
+  T = Meta.parse("Krylov." * f * "Stats{S}")
+  @eval function show(io :: IO, stats :: $T) where S
+    s  = "\n" * $f * " stats\n"
+    for field in fieldnames($T)
+      field_name = if field ∈ keys(special_fields) 
+        special_fields[field]
+      else
+        replace(string(field), "_" => " ")
+      end
+      s *=  " " * field_name * ":"
+      statfield = getfield(stats, field)
+      if typeof(statfield) <: AbstractVector && eltype(statfield) <: Union{Missing, AbstractFloat}
+        s *= @sprintf " %s\n" vec2str(statfield)
+      else
+        s *= @sprintf " %s\n" statfield
+      end
+    end
+    print(io, s)
+  end
 end
