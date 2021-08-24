3cfd9cdc7a262d480a9cf046c0c06f67802c705b
Alexis Montoison <alexis.montoison@polymtl.ca>
2021-08-24 00:45:19 -0400

    Remove last allocations in SYMMLQ

--- test/test_alloc.jl
+++ test/test_alloc.jl
@@ -24,7 +24,7 @@ function test_alloc()
   solver = SymmlqSolver(A, b)
   symmlq!(solver, A, b)  # warmup
   inplace_symmlq_bytes = @allocated symmlq!(solver, A, b)
-  @test (VERSION < v"1.5") || (inplace_symmlq_bytes ≤ 192)
+  @test (VERSION < v"1.5") || (inplace_symmlq_bytes == 0)
 
   # CG needs:
   # 4 n-vectors: x, r, p, Ap
