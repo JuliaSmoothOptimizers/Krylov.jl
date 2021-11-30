73ba03468bea01df6cd597c10dc47d9e6af85097
Alexis Montoison <alexis.montoison@polymtl.ca>
2021-11-30 14:54:05 -0500

    Loosen preconditioner eltype check

--- src/cr.jl
+++ src/cr.jl
@@ -58,7 +58,7 @@ function cr!(solver :: CrSolver{T,S}, A, b :: AbstractVector{T};
   # Check type consistency
   eltype(A) == T || error("eltype(A) ≠ $T")
   ktypeof(b) == S || error("ktypeof(b) ≠ $S")
-  MisI || (eltype(M) == T) || error("eltype(M) ≠ $T")
+  MisI || (promote_type(eltype(M), T) == T) || error("eltype(M) can't be promoted to $T")
 
   # Set up workspace
   allocate_if(!MisI, solver, :Mq, S, n)
--- src/craigmr.jl
+++ src/craigmr.jl
@@ -90,8 +90,8 @@ function craigmr!(solver :: CraigmrSolver{T,S}, A, b :: AbstractVector{T};
   # Check type consistency
   eltype(A) == T || error("eltype(A) ≠ $T")
   ktypeof(b) == S || error("ktypeof(b) ≠ $S")
-  MisI || (eltype(M) == T) || error("eltype(M) ≠ $T")
-  NisI || (eltype(N) == T) || error("eltype(N) ≠ $T")
+  MisI || (promote_type(eltype(M), T) == T) || error("eltype(M) can't be promoted to $T")
+  NisI || (promote_type(eltype(N), T) == T) || error("eltype(N) can't be promoted to $T")
 
   # Compute the adjoint of A
   Aᵀ = A'
