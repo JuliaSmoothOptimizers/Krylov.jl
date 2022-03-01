c93cdd91d5302eff1c42e6c515f59bc3aa806904
Alexis Montoison <alexis.montoison@polymtl.ca>
2022-03-01 02:08:19 -0500

    [TriMR] Support of complex numbers

--- test/test_trimr.jl
+++ test/test_trimr.jl
@@ -1,7 +1,7 @@
 @testset "trimr" begin
   trimr_tol = 1.0e-6
 
-  for FC in (Float64,)
+  for FC in (Float64, ComplexF64)
     @testset "Data Type: $FC" begin
 
       # Test underdetermined adjoint systems.
