e9364442cfbe7524a42f169d8373dbfbce1258bb
Alexis Montoison <alexis.montoison@polymtl.ca>
2022-03-01 02:23:36 -0500

    [MINRES-QLP] Support of complex numbers

--- test/test_minres_qlp.jl
+++ test/test_minres_qlp.jl
@@ -1,7 +1,7 @@
 @testset "minres_qlp" begin
   minres_qlp_tol = 1.0e-6
 
-  for FC in (Float64,)
+  for FC in (Float64, ComplexF64)
     @testset "Data Type: $FC" begin
 
       # Cubic spline matrix.
