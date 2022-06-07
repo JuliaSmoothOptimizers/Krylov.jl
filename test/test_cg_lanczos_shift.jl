a5ded54736aef75a59868ea4e8de61cd889c8099
Geoffroy Leconte <gleconte50@gmail.com>
2022-06-07 11:45:24 -0400

    cb cg_lanczos_shifts

--- test/test_cg_lanczos_shift.jl
+++ test/test_cg_lanczos_shift.jl
@@ -41,6 +41,18 @@ end
       resids = map(norm, r) / norm(b)
       @test(all(resids .≤ cg_lanczos_shift_tol))
       @test(stats.solved)
+
+      # test callback function
+      A, b = symmetric_definite(FC=FC)
+      shifts = [1.0; 2.0; 3.0; 4.0; 5.0; 6.0]
+      solver = CgLanczosShiftSolver(A, b, length(shifts))
+      tol = 1.0e-1
+      cb_n2 = TestCallbackN2Shifts(A, b, shifts, tol = tol)
+      cg_lanczos_shift!(solver, A, b, shifts, atol = 0.0, rtol = 0.0, callback = solver -> cb_n2(solver))
+      @test solver.stats.status == "user-requested exit"
+      @test cb_n2(solver)
+
+      @test_throws TypeError cg_lanczos_shift(A, b, shifts, callback = solver -> "string", history = true)
     end
   end
 end
