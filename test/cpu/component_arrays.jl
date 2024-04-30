14cd152a2c11fefdef84d48f1a5b40920d31ddbc
Alexis Montoison <35051714+amontoison@users.noreply.github.com>
2024-04-30 17:10:31 -0400

    Support ComponentArrays.jl in Krylov.jl (#859)
    * Update krylov_utils.jl
    
    * Add a test for ComponentArrays.jl
    
    * Fix .buildkite/pipeline.yml
    
    * Update component_arrays.jl

---
+++ test/cpu/component_arrays.jl
@@ -0,0 +1,15 @@
+using LinearAlgebra, SparseArrays, Test
+using Krylov, ComponentArrays
+
+@testset "ComponentArrays" begin
+  n = 5
+
+  for T in (Float32, Float64)
+    A = rand(T, n, n)
+
+    b = ComponentVector{T}(a=[1, 2, 3], b=[4, 5])
+    @test Krylov.ktypeof(b) == Vector{T}
+    x, stats = gmres(A, b)
+    @test stats.solved
+  end
+end
