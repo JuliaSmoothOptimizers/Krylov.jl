f2207e2b9c37bd45362fde644e1157072e150644
Alexis Montoison <alexis.montoison@polymtl.ca>
2024-10-13 23:44:36 -0500

    Define cg_lanczos_shift with metaprogramming

--- src/krylov_solve.jl
+++ src/krylov_solve.jl
@@ -44,9 +44,22 @@ for (KS, fun, fun2, args, def_args, optargs, def_optargs, kwargs, def_kwargs) in
   (:GpmrSolver          , :gpmr!            , :gpmr            , args_gpmr            , def_args_gpmr            , optargs_gpmr      , def_optargs_gpmr      , kwargs_gpmr            , def_kwargs_gpmr            )
 ]
   # window :: 5 -> lslq, lsmr, lsqr, minres, symmlq
-  # shifts -> CgLanczosShiftSolver(A, b, nshifts)
   # memory :: 20 -> diom, dqgmres, fom, gmres, fgmres, gpmr
-  if fun2 ∉ (:lslq, :lsmr, :lsqr, :minres, :symmlq, :cg_lanczos_shift, :diom, :dqgmres, :fom, :gmres, :fgmres, :gpmr)
+  if fun2 == :cg_lanczos_shift
+    @eval begin
+      ## Out-of-place
+      function $(fun2)($(def_args...); $(def_kwargs...)) where {T <: AbstractFloat, FC <: FloatOrComplex{T}}
+        start_time = time_ns()
+        nshifts = length(shifts)
+        solver = $KS(A, b, nshifts)
+        elapsed_time = ktimer(start_time)
+        timemax -= elapsed_time
+        $(fun)(solver, $(args...); $(kwargs...))
+        solver.stats.timer += elapsed_time
+        return results(solver)
+      end
+    end
+  elseif fun ∉ (:cg_lanczos_shift, :lslq, :lsmr, :lsqr, :minres, :symmlq, :diom, :dqgmres, :fom, :gmres, :fgmres, :gpmr)
     @eval begin
       ## Out-of-place
       function $(fun2)($(def_args...); $(def_kwargs...)) where {T <: AbstractFloat, FC <: FloatOrComplex{T}}
