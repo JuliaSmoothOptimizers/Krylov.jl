1ab71053eac84e30aa4d405533c653dd099f65a3
Alexis Montoison <alexis.montoison@polymtl.ca>
2024-11-04 01:31:49 -0600

    Test block_minres on GPUs

--- test/gpu/intel.jl
+++ test/gpu/intel.jl
@@ -86,8 +86,17 @@ include("gpu.jl")
       @test norm(b - A * x) ≤ atol + rtol * norm(b)
     end
 
+    @testset "block-MINRES -- $FC" begin
+      A, b = symmetric_indefinite(FC=FC)
+      B = hcat(b, -b)
+      A = M(A)
+      B = M(B)
+      X, stats = block_minres(A, B)
+      @test norm(B - A * X) ≤ atol + rtol * norm(B)
+    end
+
     @testset "block-GMRES -- $FC" begin
-      A, b = symmetric_definite(FC=FC)
+      A, b = nonsymmetric_definite(FC=FC)
       B = hcat(b, -b)
       A = M(A)
       B = M(B)
