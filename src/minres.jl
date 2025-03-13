7abcb4a2e469f6a0e91a011c464e28684e08b7dc
Farhad Rahbarnia <31899325+farhadrclass@users.noreply.github.com>
2025-03-13 17:28:05 -0400

    Update src/minres.jl
    Co-authored-by: Dominique <dominique.orban@gmail.com>

--- src/minres.jl
+++ src/minres.jl
@@ -323,8 +323,8 @@ kwargs_minres = (:M, :ldiv, :linesearch ,:λ, :atol, :rtol, :etol, :conlim, :itm
 
       if linesearch
         # compute the residual vector and store it in npc_dir
-        kscal!(n, sn * sn, npc_dir  )  # npc_dir  = sn * sn * npc_dir 
-        kaxpy!(n, -ϕbar * cs, v, npc_dir )   # npc_dir  = npc_dir  - ϕbar * cs * v
+        kscal!(n, sn * sn, npc_dir)  # npc_dir  = sn * sn * npc_dir 
+        kaxpy!(n, -ϕbar * cs, v, npc_dir)  # npc_dir  = npc_dir  - ϕbar * cs * v
       end
       
       # Final update of w.
