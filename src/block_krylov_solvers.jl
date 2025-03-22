15d47fe96d01ef7f0a1863fe5998723af37f5c12
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-03-22 04:28:26 -0500

    Fix a typo

--- src/block_krylov_solvers.jl
+++ src/block_krylov_solvers.jl
@@ -59,8 +59,8 @@ function BlockMinresSolver(m::Integer, n::Integer, p::Integer, SV::Type, SM::Typ
   Hₖ₋₁ = SM(undef, 2p, p)
   τₖ₋₂ = SV(undef, p)
   τₖ₋₁ = SV(undef, p)
-  SV = isconcretetype(SV) ? typeof(τₖ₋₁)
-  SM = isconcretetype(SM) ? typeof(X)
+  SV = isconcretetype(SV) ? SV : typeof(τₖ₋₁)
+  SM = isconcretetype(SM) ? SM : typeof(X)
   stats = SimpleStats(0, false, false, false, T[], T[], T[], 0.0, "unknown")
   solver = BlockMinresSolver{T,FC,SV,SM}(m, n, p, ΔX, X, P, Q, C, D, Φ, Vₖ₋₁, Vₖ, wₖ₋₂, wₖ₋₁, Hₖ₋₂, Hₖ₋₁, τₖ₋₂, τₖ₋₁, false, stats)
   return solver
@@ -121,8 +121,8 @@ function BlockGmresSolver(m::Integer, n::Integer, p::Integer, SV::Type, SM::Type
   R  = SM[SM(undef, p, p) for i = 1 : div(memory * (memory+1), 2)]
   H  = SM[SM(undef, 2p, p) for i = 1 : memory]
   τ  = SV[SV(undef, p) for i = 1 : memory]
-  SV = isconcretetype(SV) ? typeof(τ)
-  SM = isconcretetype(SM) ? typeof(X)
+  SV = isconcretetype(SV) ? SV : typeof(τ)
+  SM = isconcretetype(SM) ? SM : typeof(X)
   stats = SimpleStats(0, false, false, false, T[], T[], T[], 0.0, "unknown")
   solver = BlockGmresSolver{T,FC,SV,SM}(m, n, p, ΔX, X, W, P, Q, C, D, V, Z, R, H, τ, false, stats)
   return solver
