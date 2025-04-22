7ec375b0e16ab99bde3e30815d847c6ebb33a332
Alexis Montoison <35051714+amontoison@users.noreply.github.com>
2025-04-21 20:36:54 -0500

    Update the docstring of usymqr!

--- src/usymqr.jl
+++ src/usymqr.jl
@@ -97,7 +97,7 @@ In these calls, `kwargs` are keyword arguments of [`usymqr`](@ref).
 
 See [`UsymqrWorkspace`](@ref) for instructions on how to create the `workspace`.
 
-For a more generic interface, you can use [`krylov_workspace`](@ref) with with `method = :usymqr` to allocate the workspace,
+For a more generic interface, you can use [`krylov_workspace`](@ref) with `method = :usymqr` to allocate the workspace,
 and [`krylov_solve!`](@ref) to run the Krylov method in-place.
 """
 function usymqr! end
