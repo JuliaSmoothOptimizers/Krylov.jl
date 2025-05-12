7191100d040ccc042adf92e50362de3f99a1eb37
Farhad Rahbarnia <31899325+farhadrclass@users.noreply.github.com>
2025-05-12 16:50:26 -0400

    Update src/krylov_stats.jl
    Co-authored-by: Dominique <dominique.orban@gmail.com>

--- src/krylov_stats.jl
+++ src/krylov_stats.jl
@@ -13,7 +13,7 @@ The fields are as follows:
 - `solved`: Indicates whether the solver successfully reached convergence (`true` if solved, `false` otherwise);
 - `inconsistent`: Flags whether the system was detected as inconsistent (i.e., when `b` is not in the range of `A`);
 - `indefinite`: Flags whether the system was detected as indefinite (i.e., when `A` is not positive definite);
-- `npcCount`: The number of negative curvature directions encountered during the solve;
+- `npcCount`: The number of nonpositive curvature directions encountered during the solve;
 - `residuals`: A vector containing the residual norms at each iteration;
 - `Aresiduals`: A vector of `A'`-residual norms at each iteration;
 - `Acond`: An estimate of the condition number of matrix `A`.
